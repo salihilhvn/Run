655ffa43e38961ab74eddb405207f725752c5b83
Salih İlhan <social.salih@gmail.com>
2026-04-09 23:17:28 +0300

    gameplay_adjustments

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [Header("Settings")]
     [SerializeField] private int coinValue = 1;
-    [SerializeField] private GameObject collectEffectPrefab; // İstersen daha sonra partikül efekti ekleyebilirsin
+    [SerializeField] private GameObject collectEffectPrefab;
+
+    [Header("Animation")]
+    [SerializeField] private Vector3 rotationSpeed = new Vector3(0f, 150f, 0f); // İstersen Inspector'dan hangi eksende döneceğini ayarlayabilirsin
 
     private bool isCollected = false;
+
+    private void Update()
+    {
+        // Altını belirlenen hızda ve eksende sürekli olarak döndürür
+        transform.Rotate(rotationSpeed * Time.deltaTime, Space.World);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
